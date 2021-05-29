c4446dedcb295e351a4f10d0149c52aa86c216d2
austin shields <ajshields196@gmail.com>
2021-05-29 17:16:04 -0400

    add chart labels

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ function getAll() {
       });
 
       renderData();
+      buildCharts();
     })
     .catch(function (error) {
       console.log(error);
@@ -81,40 +82,64 @@ function renderData() {
   });
 
 }
+
+function buildCharts() {
+  buildChart("stars");
+  buildChart("watchers");
+  buildChart("forks");
+}
+
+function buildChart(name) {
+  //get context
+  let ctx = document.getElementById(`${name}Chart`).getContext('2d');
+
+  //get chart-specific data
+  let chartData = {};
+  let labels = [];
+  frameworkData.forEach(function (framework) {
+    labels.push(framework.name);
+  });
+
+  chartData.labels = labels;
+
+  renderChart(ctx, chartData);
+}
+
+function renderChart(ctx, chartData) {
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: chartData.labels,
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+}
+
 getAll();
 
 var ctx = document.getElementById('starsChart').getContext('2d');
-var myChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
-});
